24cb29b6171e4280d29af330c0afe8ee59e9d4df
Bruno Henrique Magro <brunohmagro@hotmail.com>
2025-04-04 11:00:39 -0300

    feat: add custom props and more components

--- src/styles/createStyles.tsx
+++ src/styles/createStyles.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import {
+  ActivityIndicator,
+  Button,
+  DrawerLayoutAndroid,
+  FlatList,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Modal,
+  Pressable,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+  VirtualizedList,
+} from 'react-native';
 import type { ViewStyle, TextStyle } from 'react-native';
 
-// Definição dos tipos para as props do StyledComponent
-type StyledComponentProps = {
+type StyledComponentProps<T extends object> = {
   style?: ViewStyle | TextStyle;
   children?: React.ReactNode;
-};
+} & T;
 
-const parseStyles = (styleString: string): ViewStyle | TextStyle => {
+const parseStaticStyles = (styleString: string): ViewStyle | TextStyle => {
   const styles: { [key: string]: string | number } = {};
   const rules = styleString.split(';');
 
@@ -26,42 +47,79 @@ const parseStyles = (styleString: string): ViewStyle | TextStyle => {
 
   return styles;
 };
+
+const parseStyles = (
+  styleString: TemplateStringsArray,
+  ...interpolations: Array<
+    ViewStyle | TextStyle | ((props: any) => ViewStyle | TextStyle | string)
+  >
+): ((props: any) => ViewStyle | TextStyle) => {
+  return (props: any) => {
+    const computedStyles: (ViewStyle | TextStyle)[] = [];
+
+    styleString.forEach((str, index) => {
+      // Processa a parte estática da string
+      if (str.trim()) {
+        const parsed = parseStaticStyles(str);
+        computedStyles.push(parsed);
+      }
+
+      if (interpolations[index]) {
+        const interpolation = interpolations[index];
+        const resolved =
+          typeof interpolation === 'function'
+            ? interpolation(props)
+            : interpolation;
+
+        if (typeof resolved === 'string') {
+          const lastRule = str.trim().split(';').filter(Boolean).pop();
+          if (lastRule) {
+            const [property] = lastRule.split(':').map((item) => item.trim());
+            const camelCaseProperty = property?.replace(/-([a-z])/g, (g) =>
+              String(g[1]).toUpperCase()
+            );
+
+            if (typeof camelCaseProperty === 'string') {
+              computedStyles.push({ [camelCaseProperty]: resolved });
+            }
+          }
+        } else {
+          computedStyles.push(resolved);
+        }
+      }
+    });
+
+    return StyleSheet.flatten(computedStyles);
+  };
+};
+
 const createStyledComponent = <P extends object>(
   Component: React.ComponentType<P> | React.ElementType
 ) => {
-  return (
-    styles: TemplateStringsArray | ((props: P) => ViewStyle | TextStyle)
+  return <CustomProps extends object>(
+    styles:
+      | TemplateStringsArray
+      | ((props: P & CustomProps) => ViewStyle | TextStyle),
+    ...interpolations: Array<
+      | ViewStyle
+      | TextStyle
+      | ((props: P & CustomProps) => ViewStyle | TextStyle | string)
+    >
   ) => {
     const StyledComponent = ({
       style,
       children,
       ...props
-    }: StyledComponentProps & P) => {
+    }: StyledComponentProps<P & CustomProps>) => {
       const computedStyles =
-        typeof styles === 'function' ? styles(props as P) : styles;
+        typeof styles === 'function'
+          ? styles(props as P & CustomProps)
+          : parseStyles(styles, ...interpolations)(props);
 
-      const processedStyles = Array.isArray(computedStyles)
-        ? parseStyles(computedStyles.join(''))
-        : computedStyles;
-
-      const isObject = (
-        styleObject: any
-      ): styleObject is ViewStyle | TextStyle => {
-        return (
-          styleObject &&
-          typeof styleObject === 'object' &&
-          !Array.isArray(styleObject) &&
-          styleObject !== null
-        );
-      };
-
-      const combinedStyles = StyleSheet.flatten([
-        isObject(processedStyles) ? processedStyles : {},
-        style || {},
-      ]);
+      const combinedStyles = StyleSheet.flatten([computedStyles, style]);
 
       return (
-        <Component style={combinedStyles} {...props}>
+        <Component style={combinedStyles} {...(props as P)}>
           {children}
         </Component>
       );
@@ -73,7 +131,24 @@ const createStyledComponent = <P extends object>(
 };
 
 export const styled = {
-  View: createStyledComponent(View),
+  ActivityIndicator: createStyledComponent(ActivityIndicator),
+  Button: createStyledComponent(Button),
+  DrawerLayoutAndroid: createStyledComponent(DrawerLayoutAndroid),
+  FlatList: createStyledComponent(FlatList),
+  Image: createStyledComponent(Image),
+  ImageBackground: createStyledComponent(ImageBackground),
+  KeyboardAvoidingView: createStyledComponent(KeyboardAvoidingView),
+  Modal: createStyledComponent(Modal),
+  Pressable: createStyledComponent(Pressable),
+  RefreshControl: createStyledComponent(RefreshControl),
+  SafeAreaView: createStyledComponent(SafeAreaView),
+  ScrollView: createStyledComponent(ScrollView),
+  SectionList: createStyledComponent(SectionList),
+  Switch: createStyledComponent(Switch),
   Text: createStyledComponent(Text),
+  TextInput: createStyledComponent(TextInput),
+  TouchableHighlight: createStyledComponent(TouchableHighlight),
   TouchableOpacity: createStyledComponent(TouchableOpacity),
+  View: createStyledComponent(View),
+  VirtualizedList: createStyledComponent(VirtualizedList),
 };
